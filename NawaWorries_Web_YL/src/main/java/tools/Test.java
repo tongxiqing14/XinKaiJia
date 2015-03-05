91be0090a037285707a39f223f889d8c580d438c
unknown <tongxiqing@ZTE9-user32.zte9.local>
2015-03-05 10:50:07 +0800

    test class

---
+++ NawaWorries_Web_YL/src/main/java/tools/Test.java
@@ -0,0 +1,16 @@
+package tools;
+
+/**
+ * Created by tongxiqing on 2015/3/5.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println((char)('a'+1));
+
+        System.out.println("titles/shadow_monster/"+((char)('a'+1))+".png");
+
+    }
+
+}
